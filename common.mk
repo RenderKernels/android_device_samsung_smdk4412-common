2730579bca00e23c4ad0cd812b32e218e8f59c5d
Snuzzo <jrracinfan@gmail.com>
2015-11-01 11:58:12 -0500

    Move ZIMAGE definition

--- common.mk
+++ common.mk
@@ -1,7 +1,6 @@
 PRODUCT_KERNEL_SOURCE := kernel/samsung/smdk4x12
 CROSS_COMPILE := $(ANDROID_BUILD_TOP)/toolchains/arm-cortex_a9-linux-gnueabihf-linaro_4.9/bin/arm-cortex_a9-linux-gnueabihf-
 ARCH := arm
-ZIMAGE := arch/arm/boot/zImage
 ZIP_FILES_DIR := device/samsung/smdk4412-common/zip_files_dir
 
 BOARD_KERNEL_BASE := $(shell cat device/samsung/smdk4412-common/boot.img-base)
