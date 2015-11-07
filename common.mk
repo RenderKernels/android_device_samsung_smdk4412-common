0de4d30104abc620fed6d251c3f439ca73f2f98a
Snuzzo <jrracinfan@gmail.com>
2015-11-07 15:47:32 -0500

    Revert "ARCH: Use default define"
    This reverts commit 5f845f2fbd9ef00b38c035c73b1d24b34d3dcd70.

--- common.mk
+++ common.mk
@@ -1,5 +1,6 @@
 PRODUCT_KERNEL_SOURCE := kernel/samsung/smdk4x12
 CROSS_COMPILE := $(ANDROID_BUILD_TOP)/toolchains/arm-cortex_a9-linux-gnueabihf-linaro_4.9/bin/arm-cortex_a9-linux-gnueabihf-
+ARCH := arm
 ZIP_FILES_DIR := device/samsung/smdk4412-common/zip_files_dir
 
 BOARD_KERNEL_BASE := $(shell cat device/samsung/smdk4412-common/boot.img-base)
